1d9833598cf84b4311d4f6a2329bb270d2acb458
E. Brandião <64043234+ebrandiao@users.noreply.github.com>
2021-05-29 17:20:54 -0300

    Add files via upload
    Arquivos do exercício do TP-01 da disciplina de fundamentos de C# - Desenvolvimento de .NET

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace subtracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string primeiroNumero, segundoNumero;
+            int num1, num2, subtracao;
+
+            Console.Write("Por favor digite o primeiro número: ");
+            primeiroNumero = Console.ReadLine();
+
+            Console.Write("Por favor digite o segundo número: ");
+            segundoNumero = Console.ReadLine();
+
+            num1 = Int32.Parse(primeiroNumero);
+            num2 = Int32.Parse(segundoNumero);
+
+            subtracao = num1 + num2;
+
+            Console.WriteLine("O resultado da soma dos dois números é: ", subtracao);
+        }
+    }
+}
